96ce6c5758190e048fdda5f255a2abd23c9cabea
rohnshrma <rohn11shrma@gmail.com>
2025-03-24 13:29:21 +0530

    add authentication middleware to protect routes

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const protect = (req, res, next) => {
+  let token = req.header("Authorization");
+  if (!token) {
+    return res.status(401).json({ message: "Not Authorized" });
+  }
+  try {
+    const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid Token" });
+  }
+};
+
+export default protect;
